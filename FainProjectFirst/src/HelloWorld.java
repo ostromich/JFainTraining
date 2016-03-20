6e69083da3ef3ac0bfc1fe0a1939501ec228d4ae
romvich <smertniy1@yandex.ru>
2016-03-20 18:39:06 +0200

    first commit

---
+++ FainProjectFirst/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world from inside IDE !!!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
